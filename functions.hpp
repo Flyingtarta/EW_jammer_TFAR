85f461166498408c647f48067099b73cb98766c3
Flyingtarta <guido_sastre@hotmail.com>
2020-05-29 08:57:52 -0300

    Delete functions.hpp

--- functions.hpp
+++
@@ -1,9 +0,0 @@
-class CLV
-{
-	tag = "clv";
-	class Funciones
-	{
-		class jammer_initPlayer 	{ file = "EWarfare\jammer_initPlayer.sqf"};
-		class EH_Emision 					{ file = "EWarfare\EH_Emision.sqf"};
-	};
-};
