3b6212b93827c3d4dac6b0dff0231d69e1b19874
Flyingtarta <guido_sastre@hotmail.com>
2020-05-29 08:54:53 -0300

    Update functions.hpp

--- functions.hpp
+++ functions.hpp
@@ -3,11 +3,7 @@ class CLV
 	tag = "clv";
 	class Funciones
 	{
-		class arrayAntenas			  {	file = "EWarfare\arrayAntenas.sqf"};
 		class jammer_initPlayer 	{ file = "EWarfare\jammer_initPlayer.sqf"};
 		class EH_Emision 					{ file = "EWarfare\EH_Emision.sqf"};
-		class triangulacion 			{ file = "EWarfare\triangulacion.sqf"};
-		//class AlrededorLoc 				{ file = "tools\AlrededorLocaciones.sqf"};
-		class retirada 						{ fiel  = "scripts\retirada.sqf"};
 	};
 };
